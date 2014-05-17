b660ebeda5b2a1853877c8d2f29cfe0d3dff7219
Kelp <kelp@phate.org>
2014-05-17 17:15:09 +0800

    added tests for @authorization()

--- application/tests/test_decorators.py
+++ application/tests/test_decorators.py
@@ -9,8 +9,31 @@ class TestDecorators(unittest.TestCase):
     def setUp(self):
         self.request = MagicMock()
 
-    def test_decorators_authorization_user_is_none(self):
+    def test_decorators_authorization_user_is_none_raise(self):
         self.request.user = None
+        @authorization(UserPermission.root)
+        def func(request):
+            return 'done'
+        self.assertRaises(Http403, func, self.request)
+
+    def test_decorators_authorization_user_is_root(self):
+        self.request.user.permission = UserPermission.root
+        @authorization()
+        def func(request):
+            return 'done'
+        result = func(self.request)
+        self.assertEqual(result, 'done')
+
+    def test_decorators_authorization_user_is_normal(self):
+        self.request.user.permission = UserPermission.normal
+        @authorization(UserPermission.normal)
+        def func(request):
+            return 'done'
+        result = func(self.request)
+        self.assertEqual(result, 'done')
+
+    def test_decorators_authorization_user_is_normal_raise(self):
+        self.request.user.permission = UserPermission.normal
         @authorization(UserPermission.root)
         def func(request):
             return 'done'
