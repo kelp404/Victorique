7ff5ff2660c4c76857132b6330b6a1b15ec48180
Kelp <kelp@phate.org>
2014-05-17 10:28:18 +0800

    added decorators.py

---
+++ application/decorators.py
@@ -0,0 +1,26 @@
+from exceptions import Http403
+from application.models.datastore.user_model import UserPermission
+
+
+def authorization(*permissions):
+    """
+    Authorization decorator.
+    :param level: UserPermission
+    """
+    def decorator(f):
+        def wraps(request, *args, **kwargs):
+            if not request.user:
+                raise Http403
+
+            # request.user is root
+            if request.user.permission == UserPermission.root:
+                return f(request, *args, **kwargs)
+
+            import logging
+            logging.error(request.user.dict())
+            if request.user.permission in permissions:
+                return f(request, *args, **kwargs)
+            else:
+                raise Http403
+        return wraps
+    return decorator
