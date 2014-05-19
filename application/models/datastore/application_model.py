15b4132b06e51429db336c50e0342a0e63ce8f74
Kelp <kelp@phate.org>
2014-05-19 10:39:33 +0800

    added ApplicationModel.

---
+++ application/models/datastore/application_model.py
@@ -0,0 +1,22 @@
+from google.appengine.ext import db
+from application import utils
+
+
+class ApplicationModel(db.Model):
+    title = db.StringProperty(indexed=False)
+    description = db.TextProperty()
+    app_key = db.StringProperty()
+    roots = db.ListProperty(long, default=[])
+    members = db.ListProperty(long, default=[])
+    create_time = db.DateTimeProperty(auto_now_add=True)
+
+    def dict(self):
+        return {
+            'id': self.key().id() if self.has_key() else None,
+            'title': self.title,
+            'description': self.description,
+            'app_key': self.app_key,
+            'roots': self.roots,
+            'members': self.members,
+            'create_time': utils.get_iso_format(self.create_time),
+        }
