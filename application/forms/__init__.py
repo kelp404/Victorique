8709a2958c550ca788907a2350ae5048b8a1708a
Kelp <kelp@phate.org>
2014-06-13 23:54:45 +0800

    added ArrayField at forms

--- application/forms/__init__.py
+++ application/forms/__init__.py
@@ -13,3 +13,12 @@ class DictField(Field):
             self.data = value
         elif isinstance(value, basestring):
             self.data = json.loads(value)
+
+class ArrayField(Field):
+    def _value(self):
+        return self.data
+    def process_data(self, value):
+        if value and isinstance(value, list):
+            self.data = value
+        else:
+            self.data = []
